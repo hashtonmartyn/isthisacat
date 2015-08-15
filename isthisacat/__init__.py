54e3d852f2c1fd2bad012d4d961b84a92beba801
Henry <henry.ashton.martyn@gmail.com>
2015-08-15 23:19:59 +1200

    The path to the schema should also probably be absolute

--- isthisacat/__init__.py
+++ isthisacat/__init__.py
@@ -36,7 +36,10 @@ def close_connection(exception):
         
 def init_db():
     with closing(connect_to_database()) as db:
-        with app.open_resource("schema.sql", mode="r") as f:
+        head, tail = os.path.split(os.path.realpath(__file__))
+        schema_path = os.path.join(head,
+                                   "schema.sql")
+        with app.open_resource(schema_path, mode="r") as f:
             db.cursor().executescript(f.read())
         db.commit()
 
